e2ff7eef67a282056fe1bbe332e04f49eb6b73b2
ramirez.aranda.dante.israel@gmail.com <dramireza1802@alumno.ipn.mx>
2024-06-06 16:01:17 -0600

    proyecto1

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar y Footer</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar">
+        <div class="logo">
+            <img src="img/logo.jpeg" alt="Logo de la Empresa">
+        </div>
+        <ul class="nav-links">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Registro</a></li>
+            <li><a href="#">Sesión</a></li>
+        </ul>
+    </nav>
+
+    <!-- Contenido principal -->
+    <div class="content">
+        <h1 class="text-center">PLOMEROS EN CDMX</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        
+        <div class="grid-container">
+            <div class="grid-item">
+                <img src="img/mantenimientotinacos.jpeg" alt="Imagen 1">
+                <p>Mantenimiento de tinacos</p>
+            </div>
+            <div class="grid-item">
+                <img src="img/consulta.jpeg" alt="Imagen 2">
+                <p>Consulta</p>
+            </div>
+            <div class="grid-item">
+                <img src="img/plomeria.jpeg" alt="Imagen 3">
+                <p>Servicio de urgencia</p>
+            </div>
+            <div class="grid-item">
+                <img src="img/instalacion.jpeg" alt="Imagen 4">
+                <p>Instalacion de tinacos</p>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <div class="gray-section">
+            <div class="content">
+                <div class="left-side">
+                    <h2>Texto en el lado izquierdo</h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="right-side">
+                    <img src="img/plomero.jpeg" alt="Imagen grande">
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <footer class="footer">
+        <p></p>
+    </footer>
+</body>
+</html>
